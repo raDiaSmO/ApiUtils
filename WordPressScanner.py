fadc1cfdb8bd5ee909c8362dfdb807ad7b248c38
raDiaSmO <antbsvrt@protonmail.com>
2022-06-18 19:25:22 -0400

    Output file added

--- WordPressScanner.py
+++ WordPressScanner.py
@@ -9,23 +9,24 @@ def wpscan():
 
     file = sys.argv[1]
     api = sys.argv[2]
+    out = sys.argv[3]
+    paths = [sys.argv[1],sys.argv[3]]
 
-    if os.path.exists(file):
-        print ('The following list will be used', os.path.abspath(file))
+    for path in paths:
 
-    else:
-        print ('The provided path does not exist.')
-        sys.exit()
+        if not os.path.exists(path):
+            print ('The provided path does not exist.',path)
+            sys.exit()
 
-    with open (file) as assets:
-        for asset in assets:
+        with open (file) as assets:
+            for asset in assets:
 
-            try:
-                fstring = f"wpscan --url {asset} --random-user-agent --disable-tls-checks --detection-mode aggressive --api-token {api} -e vp,vt,dbe --output ~/wpscanresult --format cli"
-                command = re.sub('\?|\!|\'|\n|\'|\;', '', fstring)
-                subprocess.check_output(['bash','-c', command])
+                try:
+                    fstring = f"wpscan --url {asset} --random-user-agent --disable-tls-checks --detection-mode aggressive --api-token {api} -e vp,vt,dbe --output {out} --format cli"
+                    command = re.sub('\?|\!|\'|\n|\'|\;', '', fstring)
+                    subprocess.check_output(['bash','-c', command])
 
-            except:
-                print ('Could not execute the request')
-                sys.exit()
+                except:
+                    print ('Could not execute the request')
+                    sys.exit()
 wpscan()
