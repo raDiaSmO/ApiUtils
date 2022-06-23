5eb9bd49a37c78fbbb43476876618ffbf657538c
raDiaSmO <antbsvrt@protonmail.com>
2022-06-23 19:59:01 -0400

    Loop added

--- CheckAbuseIp.py
+++ CheckAbuseIp.py
@@ -7,23 +7,27 @@ import sys
 def check_ip():
 
     endpoint = 'https://api.abuseipdb.com/api/v2/check'
+    file = sys.argv[1]
 
-    ip = {
-                'ipAddress': sys.argv[1]
-    }
+    with open (file) as ips:
+        for ip in ips:
 
-    http_headers = {
-            'Accept': 'application/json',
-            'Key': sys.argv[2]
-    }
+            data = {
+                        'ipAddress': ip
+            }
 
-    try:
-        r = requests.request(method='GET', url=endpoint, headers=http_headers, params=ip)
-        decoded_r = json.loads (r.text)
-        print ( json.dumps (decoded_r, sort_keys=True, indent=3) )
+            http_headers = {
+                    'Accept': 'application/json',
+                    'Key': sys.argv[2]
+            }
 
-    except:
-        print ('Could not execute the following request.')
-        sys.exit()
+            try:
+                r = requests.request(method='GET', url=endpoint, headers=http_headers, params=data)
+                decoded_r = json.loads (r.text)
+                print ( json.dumps (decoded_r, sort_keys=True, indent=3) )
+
+            except:
+                print ('Could not execute the following request.')
+                sys.exit()
 
 check_ip()
